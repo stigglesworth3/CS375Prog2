f87a4bc01a4c8ee4a1666ef1990c9c6c3ff7b7ac
Kyle Stigler <kstigle1@binghamton.edu>
2020-04-12 15:31:50 -0400

    read in/split/stored everything

--- BestFirstSearch.cpp
+++ BestFirstSearch.cpp
@@ -1,8 +1,16 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 using namespace std;
+
+struct singleItem
+{
+	int weight;
+	int profit;
+};
+typedef struct singleItem item;
 
 int main(int argc, char *argv[])
 {
@@ -15,9 +23,21 @@ int main(int argc, char *argv[])
 
 	size_t comma = toBeSplit.find(",");
 	string totalItems = toBeSplit.substr(0, comma);
-	string totalWeight =toBeSplit.substr(comma+1);
+	string totalWeight = toBeSplit.substr(comma+1);
 	int numItems = stoi(totalItems);
 	int sackWeight = stoi(totalWeight);
+
+	vector<item> items;
+
+	for (int i=0; i<numItems; i++)
+	{
+		item nItem;
+		inputFile >> toBeSplit;
+		comma = toBeSplit.find(",");
+		nItem.weight = stoi(toBeSplit.substr(0, comma));
+		nItem.profit = stoi(toBeSplit.substr(comma+1));
+		items.push_back(nItem);
+	}
 
 	outFile.close();
 	return 0;
