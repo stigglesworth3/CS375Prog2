68bc3d10e5c5aaadd9cf2a3b2821e874ced0dd56
Kyle Stigler <kstigle1@binghamton.edu>
2020-04-13 16:48:06 -0400

    optimal profit found

--- BestFirstSearch.cpp
+++ BestFirstSearch.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <queue>
 
 using namespace std;
+
+int numLeaves = 0;
+int numNodes = 0;
 
 struct item
 {
 	double weight;
 	double profit;
 	double profitToWeight;
+};
+
+struct Node
+{
+	int level;
+	double profit;
+	double bound;
+	double weight;
 };
 
 void bSort(item items[], int size)
@@ -27,6 +39,83 @@ void bSort(item items[], int size)
 	}
 }
 
+int bound(Node v, int numItems, int sackWeight, item items[])
+{
+	if (v.weight >= sackWeight)
+	{
+		return 0;
+	}
+	double profitBound = v.profit;
+	int nLevel = v.level+1;
+	int totalWeight = v.weight;
+
+	while ((nLevel < numItems) && (totalWeight + items[nLevel].weight <= sackWeight))
+	{
+		totalWeight += items[nLevel].weight;
+		profitBound += items[nLevel].profit;
+		nLevel++;
+	}
+
+	if (nLevel < numItems)
+	{
+		profitBound += (sackWeight - totalWeight) * items[nLevel].profitToWeight;
+	}
+
+	return profitBound;
+}
+
+int knapsack01(int sackWeight, item items[], int numItems)
+{
+	queue<Node> q;
+	Node curr, next;
+
+	curr.level = -1;
+	curr.profit = curr.weight = 0;
+	q.push(curr);
+
+	int maxProfit = 0;
+	while (!q.empty())
+	{
+		curr = q.front();
+		q.pop();
+
+		if (curr.level == -1)
+		{
+			next.level = 0;
+		}
+		if (curr.level == numItems-1)
+		{
+			numLeaves++;
+			continue;
+		}
+
+		next.level = curr.level+1;
+		next.weight = curr.weight + items[next.level].weight;
+		next.profit = curr.profit + items[next.level].profit;
+
+		if (next.weight <= sackWeight && next.profit > maxProfit)
+		{
+			maxProfit = next.profit;
+		}
+
+		next.bound = bound(next, numItems, sackWeight, items);
+		
+		if (next.bound > maxProfit)
+		{
+			q.push(next);
+		}
+
+		next.weight = curr.weight;
+		next.profit = curr.profit;
+		next.bound = bound(next, numItems, sackWeight, items);
+		if (next.bound > maxProfit)
+		{
+			q.push(next);
+		}
+	}
+	return maxProfit;
+}
+
 int main(int argc, char *argv[])
 {
 	ifstream inputFile(argv[1]);
@@ -41,6 +130,8 @@ int main(int argc, char *argv[])
 	string totalWeight = toBeSplit.substr(comma+1);
 	int numItems = stoi(totalItems);
 	int sackWeight = stoi(totalWeight);
+	
+	outFile << numItems << ",";
 
 	item items[numItems];
 
@@ -56,6 +147,9 @@ int main(int argc, char *argv[])
 		items[i] = nItem;
 	}
 	bSort(items, numItems);
+	int optProfit = knapsack01(sackWeight, items, numItems);
+	outFile << optProfit << "," << endl;
+	outFile << numLeaves << endl;
 
 	outFile.close();
 	return 0;
