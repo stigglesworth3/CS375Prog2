452da63b6283b36c88bef10690f04e5141721046
Kyle Stigler <kstigle1@binghamton.edu>
2020-04-13 15:25:37 -0400

    inputs sorted

--- BestFirstSearch.cpp
+++ BestFirstSearch.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 #include <string>
 #include <fstream>
-#include <vector>
 
 using namespace std;
 
 struct singleItem
 {
-	int weight;
-	int profit;
+	double weight;
+	double profit;
 	double profitToWeight;
 };
 typedef struct singleItem item;
+
+void bSort(item items[], int size)
+{
+	for (int i=0; i<size-1; i++)
+	{
+		for (int j=0; j<size-1-i; j++)
+		{
+			if (items[j].profitToWeight < items[j+1].profitToWeight)
+			{
+				item temp = items[j];
+				items[j] = items[j+1];
+				items[j+1] = temp;
+			}
+		}
+	}
+}
 
 int main(int argc, char *argv[])
 {
@@ -28,25 +43,26 @@ int main(int argc, char *argv[])
 	int numItems = stoi(totalItems);
 	int sackWeight = stoi(totalWeight);
 
-	vector<item> items;
+	item items[numItems];
 
 	for (int i=0; i<numItems; i++)
 	{
 		item nItem;
 		inputFile >> toBeSplit;
 		comma = toBeSplit.find(",");
-		nItem.weight = stoi(toBeSplit.substr(0, comma));
-		nItem.profit = stoi(toBeSplit.substr(comma+1));
+		nItem.weight = stod(toBeSplit.substr(0, comma));
+		nItem.profit = stod(toBeSplit.substr(comma+1));
 		nItem.profitToWeight = (nItem.profit)/(nItem.weight);
-		items.push_back(nItem);
+		cout << nItem.profitToWeight << endl;
+		items[i] = nItem;
 	}
+	bSort(items, numItems);
 
-	for (int k=0; k<items.size(); k++)
+	for (int k=0; k<numItems; k++)
 	{
-		outFile << items[k].profitToWeight;
+		outFile << items[k].profitToWeight << endl;
 	}
 
 	outFile.close();
 	return 0;
 }
-//can i pull this?
